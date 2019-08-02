ba24c4ea0fc14568136b315f8ac5348ec6e4e0b9
筱傑 <840465812@qq.com>
2019-08-02 16:19:14 +0800

    Add MvvmLight Template

---
+++ MvvmLightDemo/ViewModel/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Input;
+
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace MvvmLightDemo.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string _Title;
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        public ICommand ChangeTitleCommand { get; set; }
+
+        public MainViewModel()
+        {
+            Title = "Hello World!";
+            ChangeTitleCommand = new RelayCommand(ChangeTitle);
+        }
+
+        private void ChangeTitle()
+        {
+            Title = "Hello MvvmLight!";
+        }
+    }
+}
---
+++ MvvmLightDemo/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,21 @@
+﻿using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace MvvmLightDemo.ViewModel
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
+
+        public static void Cleanup()
+        {
+            // TODO Clear the ViewModels
+        }
+    }
+}
